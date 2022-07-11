465e908e3e1d957cd99b936104d9ee15a5e0e8af
mandugo <frankirchhoff@gmail.com>
2022-07-11 10:21:02 +0200

    Function hexificator() modified

--- siRad.py
+++ siRad.py
@@ -1,19 +1,20 @@
 #https://pypi.org/project/pyserial/
-
+from textwrap import wrap
 
 # https://stackoverflow.com/a/21872007
 def twosComplement(val):
     return format(val % (1 << 16), '016b')
 
-def hexify(bits):
-    return hex(int(bits,2))[2:]
+def hexificator(bits):
+    splittedString = wrap(bits, 4)
+    hexStrings = [hex(int(subString,2))[2:] for subString in splittedString]
+    return ''.join(hexStrings)
 
 def bandwidthConfiguration(BW):
     start = "!P"
     stop = "\r\n"
     val = int(BW/2)
     bitString = '0000000000000000' + twosComplement(val)
-    command = hexify(bitString[0:4]) + hexify(bitString[4:8]) + hexify(bitString[8:12]) + hexify(bitString[12:16]) + hexify(bitString[16:20]) + hexify(bitString[20:24]) + hexify(bitString[24:28]) + hexify(bitString[28:])   
-    fullCommand = start + command + stop
+    fullCommand = start + hexificator(bitString) + stop
     return fullCommand
 
