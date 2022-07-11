c0d3e9e769b88622a5a530a0ceeba026ac66cab9
mandugo <frankirchhoff@gmail.com>
2022-07-11 12:38:23 +0200

    Added some error controls

--- siRad.py
+++ siRad.py
@@ -7,24 +7,30 @@ import math
 
 # https://stackoverflow.com/a/21872007
 def twosComplement(val):
-    return format(val % (1 << 16), '016b')
+    if isinstance(val, int):
+        return format(val % (1 << 16), '016b')
+    else:
+        raise TypeError("The input must be an integer.")
 
 def hexificator(bits):
-    if isinstance(bits, str):
+    if (isinstance(bits, str) and (len(bits) % 4) == 0):
         splittedString = wrap(bits, 4)
         hexStrings = [hex(int(subString,2))[2:] for subString in splittedString]
         return ''.join(hexStrings)
     else:
-        raise TypeError("The input must be a string.")
+        raise TypeError("The input must be a string and it must contain a number of bits multiple of four.")
 
 def bandwidthConfiguration(BW=1000):
-    if BW <= 65534 and BW >= -65536:
-        val = int(BW/2)
-        bitString = '0000000000000000' + twosComplement(val)
-        fullCommand = "!P" + hexificator(bitString) + "\r\n"
-        return fullCommand
+    if isinstance(BW, int):
+        if BW <= 65534 and BW >= -65536:
+            val = int(BW/2)
+            bitString = '0000000000000000' + twosComplement(val)
+            fullCommand = "!P" + hexificator(bitString) + "\r\n"
+            return fullCommand
+        else:
+            raise ValueError("The bandwidth must be a number between -65536 MHz and 65534 MHz.")
     else:
-        raise ValueError("The bandwidth must be a number between -65536 MHz and 65534 MHz.")
+        raise TypeError("The input must be an integer.")
 
 def selfTrigDelaySetting(delay=0):
     if isinstance(delay, int):
