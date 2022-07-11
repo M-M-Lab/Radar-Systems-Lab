6497ff4972906056210295524e2df11e3b7c64ae
mandugo <frankirchhoff@gmail.com>
2022-07-11 11:13:48 +0200

    Added findSerialDevice() to find the radar serial port

--- siRad.py
+++ siRad.py
@@ -1,5 +1,8 @@
 #https://pypi.org/project/pyserial/
 from textwrap import wrap
+import serial
+
+# ---------- COMMANDS FORMATTING ------------ #
 
 # https://stackoverflow.com/a/21872007
 def twosComplement(val):
@@ -18,3 +21,11 @@ def bandwidthConfiguration(BW):
         return fullCommand
     else:
         raise ValueError("The bandwidth must be a number between -65536 MHz and 65534 MHz.")
+
+# ---------- SERIAL HANDLING ------------ #
+
+def findSerialDevice(hwID="pappappero"):
+    radarBoard = serial.tools.list_ports.grep(hwID)
+    print(radarBoard.description) #debug purposes
+    return radarBoard.device
+
