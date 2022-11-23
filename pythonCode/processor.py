05ee937ce0e8adc2fef093a0544721bb07035b16
Rustafun <giuliomeucci@gmail.com>
2022-11-23 09:57:41 +0100

    Migliorie alla leggibilità e aggiunta di commenti

--- pythonCode/processor.py
+++ pythonCode/processor.py
@@ -5,27 +5,33 @@ import json
 
 def process(radar2processor_dataQ,processor2GUI_dataQ,GUI2processor_controlQ):
 
-    NSweepIntegration=16
+    NSweepIntegration=16    #Numero di sweep su cui viene fatta la mappa RD
+    
+    #Init
     dump2File=False
     hamming=[]
     time.sleep(0.5)
+    
     while True:
         while GUI2processor_controlQ.qsize():
             key,value=GUI2processor_controlQ.get()
-            if key=='Samps':
+            #Change buffer size depending on GUI queue 
+            if key=='Samps':                            
                 buffSize=int(value)
+            
+            #Save to file
             elif key=='Dump':
-                if value:
-                    #Init recording
+                if value:   #Init recording
                     dataDict=value #??
                     dump2File=True
                     listOfFrame=[]
                     listOfTStamp=[]
+
                     #Create name for dump file with timestamp
                     date=time.asctime()
                     date=date.replace(' ','_')
                     fileName='../dump/dump_'+date.replace(':','')+'.json'
-                else:
+                else:       #Dump to file
                     f = open(fileName, 'a')
                     dataDict['data']=listOfFrame
                     dataDict['tStamp']=listOfTStamp
@@ -34,19 +40,19 @@ def process(radar2processor_dataQ,processor2GUI_dataQ,GUI2processor_controlQ):
                     dump2File=False
 
 
-        complexData=np.zeros(buffSize)
+        complexData=np.zeros(buffSize)                                     #Maybe useless!!!
 
+        #Weighting window definition
         if len(hamming)!=buffSize:
             hammingCol=np.hamming(buffSize)
             hammingRow=np.hamming(NSweepIntegration)
             hamming=hammingCol[:,np.newaxis]*hammingRow[np.newaxis,:]
 
-        RDMap=np.zeros((int(buffSize),NSweepIntegration),dtype=np.complex)
+        RDMap=np.zeros((int(buffSize),NSweepIntegration),dtype=np.complex)  #Maybe useless!!!
 
         startTime=time.time()
         for ind in range(NSweepIntegration):
             buf,tStamp=radar2processor_dataQ.get()
-            #buffSize=int(int.from_bytes(buf[7:8],"big")/2)
             if len(buf[9:-2])>=2*buffSize:    
                 buf=struct.iter_unpack('<h',buf[9:-2])
                 buf=[val[0] for val in buf]
@@ -56,28 +62,20 @@ def process(radar2processor_dataQ,processor2GUI_dataQ,GUI2processor_controlQ):
                 if dump2File:
                     listOfFrame.append(buf)
                     listOfTStamp.append(tStamp)
-                #complexData=complexData-np.mean(complexData)
             RDMap[:,ind]=complexData
         PRI=(time.time()-startTime)/NSweepIntegration
         
         #Hamming
         RDMap=RDMap*hamming
         
-        #FFT 1
+        #FFT (TODO: implement zeropadding selector)
         RangeMap=np.fft.fft(RDMap,2*buffSize,axis=0)
-        #rowPhase=np.exp(1j*np.mean(np.angle(RangeMap),0))
-        #colPhase=np.exp(1j*np.mean(np.angle(RangeMap),1))
-        #meanPhaseMat=rowPhase[np.newaxis,:]*colPhase[:,np.newaxis]
-        #RangeMap=RangeMap*np.conj(meanPhaseMat)#*np.conj(colPhase[:,np.newaxis])
         C=np.fft.fftshift(np.fft.fft(RangeMap,NSweepIntegration*4,axis=1),1)
-        
-        #FFT 2
-        #C=np.fft.fftshift(np.fft.fft2(RDMap),1)
         
         #Cut RDMap in half
         C=C[int(buffSize):,:]
-        C[C == 0] = np.min(C[C != 0]) #MODIFIED -> = 1
+        C[C == 0] = np.min(C[C != 0]) #MODIFIED -> = 1 [rimuove gli zeri per evitare problemi con la np.log10(.)]
         C=np.log10(np.abs(C))
         
-        #print('Proc Q', processor2GUI_dataQ.qsize())
+        #print('Proc Q', processor2GUI_dataQ.qsize()) #Debug, mostra la lunghezza della coda tra processore e interfaccia grafica
         processor2GUI_dataQ.put((C,PRI))
